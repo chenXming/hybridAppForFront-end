6fba4d3dea13f1e4e8f0b90f4e6b5762411d72e8
chenxiaoming <chenxiaoming@360.cn>
2021-08-17 10:44:21 +0800

    toolbar 功能完成

---
+++ hybird-app/src/components/ToolBar.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="tool-bar">
+        <div class="tool-bar-item" v-for="(item,index) in toolBarData" :key="index">
+            <img class="tool-bar-item-img" :src="[index === selecItemIndex? item.hIocn: item.nIocn]" alt="" />
+            <!-- 添加字体高亮类，后面是展示条件 -->
+            <p class="tool-bar-item-name" :class="{'tool-bar-item-name-h' : index === selecItemIndex}">{{item.name}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+// 他自己在页面的位置 应该又外部调用的业务来确定
+    export default {
+        data() {
+            return {// vue\react\angular MVVM 又数据驱动视图
+                toolBarData: [
+                    {//默认图片
+                        nIocn:require('@img/home-n.svg'),
+                        hIocn:require('@img/home-h.svg'),
+                        name: "首页",
+                        //展示组件名称
+                        commponentsName:'home'
+                    },
+                    {
+                        //默认图片
+                        nIocn:require('@img/shopping-n.svg'),
+                        hIocn:require('@img/shopping-h.svg'),
+                        name: "购物车",
+                        //展示组件名称
+                        commponentsName:'shopping'
+                    },
+                    {
+                        //默认图片
+                        nIocn:require('@img/my-n.svg'),
+                        hIocn:require('@img/my-h.svg'),
+                        name: "我的",
+                        //展示组件名称
+                        commponentsName:'my'
+                    },
+                   
+                ],
+                selecItemIndex: 0,
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+@import '@css/style.scss';
+
+   .tool-bar {
+        width: 100%;
+        height: px2rem(46);
+        display: flex;
+        justify-content: space-around;
+        background-color: white;
+        box-shadow: 0 0 16px 0 rgba(0, 0, 0, 0.2);
+        border-top: 1px solid $lineColor;
+
+        &-item {
+            text-align: center;
+            padding: px2rem(4) px2rem(12);
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+
+            &-img {
+                width: px2rem(22);
+                height: px2rem(22);
+            }
+
+            &-name {
+                font-size: $infoSize;
+                margin-top: px2rem(4);
+
+                &-h {
+                    color: $mainColor;
+                }
+            }
+        }
+
+    }
+</style>
---
+++ hybird-app/src/views/Main.vue
@@ -0,0 +1,26 @@
+<template>
+<!-- 在 vue这种单页面程序中 ，只存在一个页面（App.vue），其他的都是组件 -->
+    <div>
+        <tool-bar></tool-bar>
+    </div>
+</template>
+
+<script>
+import ToolBar from '@c/ToolBar.vue';
+
+    export default {
+        name: 'main',
+        components: {
+            ToolBar,
+        },
+        data() {
+            return {
+
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
